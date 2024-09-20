6224eb66bb3b22948798837717b819a01a64acea
Пётр Шкляр <po_scripty@MBP-Petr.lan>
2024-09-20 22:46:00 +0300

    due

--- src/Main.java
+++ src/Main.java
@@ -26,12 +26,12 @@ public class Main {
                 return;
             }
 
-            // Вызов всех функций из NumberOperations
-            int max = NumberOperations.findMax(numbers);
-            int min = NumberOperations.findMin(numbers);
-            int sum = NumberOperations.calculateSum(numbers);
-            double average = NumberOperations.calculateAverage(numbers);
-            List<Integer> evenNumbers = NumberOperations.findEvenNumbers(numbers);
+            // Вызов всех функций
+            int max = findMax(numbers);
+            int min = findMin(numbers);
+            int sum = calculateSum(numbers);
+            double average = calculateAverage(numbers);
+            List<Integer> evenNumbers = findEvenNumbers(numbers);
 
             // Вывод результатов
             System.out.println("Максимальное число: " + max);
@@ -43,5 +43,62 @@ public class Main {
         } catch (FileNotFoundException e) {
             System.out.println("Файл number.txt не найден.");
         }
+    }
+
+    // Найти максимальное число в списке
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (int num : numbers) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    // Найти минимальное число в списке
+    public static int findMin(List<Integer> numbers) {
+        int min = numbers.get(0);
+        for (int num : numbers) {
+            if (num < min) {
+                min = num;
+            }
+        }
+        return min;
+    }
+
+    // Посчитать сумму всех чисел в списке
+    public static int calculateSum(List<Integer> numbers) {
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    // Посчитать "среднее арифметическое" (с ошибкой)
+    public static double calculateAverage(List<Integer> numbers) {
+        int product = 1;
+        for (int num : numbers) {
+            product *= num;
+        }
+        return (double) product / numbers.size();
+    }
+
+
+//    public static double calculateAverage(List<Integer> numbers) {
+//        int sum = calculateSum(numbers);
+//        return (double) sum / numbers.size();
+//    }
+
+    // Найти все четные числа в списке
+    public static List<Integer> findEvenNumbers(List<Integer> numbers) {
+        List<Integer> evenNumbers = new ArrayList<>();
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                evenNumbers.add(num);
+            }
+        }
+        return evenNumbers;
     }
 }
